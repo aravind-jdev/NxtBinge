dc89ac63350bd36135f574a4da8542c230a3f338
aravind-jdev <aravind18l15@gmail.com>
2026-03-26 13:34:20 +0530

    api url updated

--- frontend/src/api/movieApi.js
+++ frontend/src/api/movieApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1/movies";
+const BASE_URL = "https://nxt-binge-backend.vercel.app/api/v1/movies";
 
 /* 🎬 Get Trending Movies */
 export const getTrendingMovies = async () => {
